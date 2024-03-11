722a6912ff2870ad6ca5920ab4fc62140559b92e
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-11 15:37:55 +0700

    feat: movie context

---
+++ src/context/movie-context.tsx
@@ -0,0 +1,53 @@
+import { ReactNode, createContext, useReducer } from "react";
+import { MovieDataType, moviesData } from "../constants/data";
+
+interface MovieContextProps {
+  children: ReactNode;
+}
+
+interface MovieState {
+  movies: MovieDataType[];
+}
+
+interface MovieAction {
+  type: string;
+  id: string;
+}
+
+const initialMovieState: MovieState = {
+  movies: moviesData,
+};
+
+const MovieReducer = (state: MovieState, action: MovieAction): MovieState => {
+  switch (action.type) {
+    case "TOOGLE BOOKMARK":
+      return {
+        ...state,
+        movies: state.movies.map((movie) => {
+          return movie.id === action.id
+            ? { ...movie, isBookmarked: !movie.isBookmarked }
+            : movie;
+        }),
+      };
+    default:
+      return state;
+  }
+};
+
+export const MovieContext = createContext<{
+  state: MovieState;
+  dispatch: React.Dispatch<MovieAction>;
+}>({
+  state: initialMovieState,
+  dispatch: () => {},
+});
+
+export const MovieProvider = ({ children }: MovieContextProps) => {
+  const [state, dispatch] = useReducer(MovieReducer, initialMovieState);
+
+  return (
+    <MovieContext.Provider value={{ state, dispatch }}>
+      {children}
+    </MovieContext.Provider>
+  );
+};
